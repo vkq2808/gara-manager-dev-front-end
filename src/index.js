c935b4ecd10a547febd54a61e1aab9ce20df0390
vkq2808 <vkq0919309031@gmail.com>
2024-09-13 21:07:25 +0700

    Refactor environment variables and update gitignore and package.json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import DataProvider from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <DataProvider>
         <App />
